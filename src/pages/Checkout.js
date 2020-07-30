5862231cdc76a903fc841742dd2a5fa87c9d364e
hackko <j.kome@hotmail.com>
2020-07-29 22:57:11 -0500

    react aws book store
    react aws book store

---
+++ src/pages/Checkout.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { AmplifyAuthenticator } from '@aws-amplify/ui-react';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import CheckoutForm from "../components/CheckoutForm";
+
+const Checkout = () => {
+    const stripePromise = loadStripe('pk_test_51HAKYgLqOmBfYfHW0UAvurcrd2LqzNf5D8RvBeOoDCaZwF8Ac7RYeLbAv2a6UzFRP0jVs2D92QRs39fNgM3gTufC00wuuz7Iwt');
+
+    return (
+        <section className="checkout-wrapper">
+            <AmplifyAuthenticator>
+                <Elements stripe={stripePromise}>
+                    <section>
+                        <h2>Time to Checkout?</h2>
+                        <CheckoutForm />
+                    </section>
+                </Elements>
+            </AmplifyAuthenticator>
+        </section>
+    )
+}
+
+export default Checkout
